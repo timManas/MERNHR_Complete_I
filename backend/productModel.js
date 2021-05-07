17ffe647915a2069256dc8a3c080859352ae5565
Tim M <timothy.romero.manas@gmail.com>
2021-05-07 18:19:06 -0400

    Added review schema'

--- backend/productModel.js
+++ backend/productModel.js
@@ -2,6 +2,13 @@ import express from 'express'
 import asyncHandler from 'express-async-handler'
 import mongoose from 'mongoose'
 
+const reviewSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+})
+
 const productSchema = mongoose.Schema({
   rating: { type: Number, required: true },
   numReviews: { type: String, required: true },
@@ -13,6 +20,7 @@ const productSchema = mongoose.Schema({
   brand: { type: String, required: true },
   category: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  review: [reviewSchema],
 })
 
 const Product = mongoose.model('Product', productSchema)
