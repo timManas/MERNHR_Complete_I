73d37f2c9900aa3e278c7414af7aeb34027d2fbf
Tim M <timothy.romero.manas@gmail.com>
2021-05-11 19:50:30 -0400

    Able to pull data from store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,18 @@
-import React, { useDispatch, useSelector } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.listProducts)
+
+  const { products } = productList
+  console.log('ProductList: ' + JSON.stringify(products))
+
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [dispatch])
+
   return (
     <>
       <h1>HomeScreen1</h1>
---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { listProductReducer } from './reducers/productReducers'
+
+const reducers = combineReducers({
+  listProducts: listProductReducer,
+})
+
+const middleware = [thunk]
+
+const initialState = {}
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
