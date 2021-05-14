18b4c3ca7513f8aa362dffdd726791c150fd443e
Tim M <timothy.romero.manas@gmail.com>
2021-05-13 23:20:54 -0400

    Added LinkContainer to product Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Product = ({ product }) => {
   return (
+    // <LinkContainer to={`/product/${product._id}`}>
+    //   <Card>
+    //     <Card.Img variant='top' src={product.image} />
+    //     <Card.Body>
+    //       <Card.Title>{product.name}</Card.Title>
+    //       <Card.Text>{product.price}</Card.Text>
+    //     </Card.Body>
+    //   </Card>
+    // </LinkContainer>
+
     <Card>
-      <Card.Img variant='top' src={product.image} />
-      <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text>{product.price}</Card.Text>
-      </Card.Body>
+      <LinkContainer to={`/product/${product._id}`}>
+        <Card.Img variant='top' src={product.image} />
+      </LinkContainer>
+      <LinkContainer to={`/product/${product._id}`}>
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>{product.price}</Card.Text>
+        </Card.Body>
+      </LinkContainer>
     </Card>
   )
 }
