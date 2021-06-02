2d40121218bec8d20b595507b8a5b5d128fe5c76
Tim M <timothy.romero.manas@gmail.com>
2021-06-01 21:40:06 -0400

    Created template for RegisterScreen

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterScreen = ({ match, location, history }) => {
+  return (
+    <>
+      <h1>RegisterScreen</h1>
+    </>
+  )
+}
+
+export default RegisterScreen
