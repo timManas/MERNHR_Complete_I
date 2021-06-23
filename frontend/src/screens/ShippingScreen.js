d0d54a9bce3bdce996a1597305a4f8a1ab5440e2
Tim M <timothy.romero.manas@gmail.com>
2021-06-22 21:29:04 -0400

    Adding Shipping Screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,7 +1,36 @@
-const ShippingScreen = () => {
+import React from 'react'
+import {Form, Button, Col} from 'react-bootstrap'
+
+const ShippingScreen = ({ location, history, match }) => {
   return (
     <>
       <h1>Shipping</h1>
+      <Form>
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="Enter Address" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>City</Form.Label>
+          <Form.Control placeholder="Enter City" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridPassword">
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control  placeholder="Enter Postal Code" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridPassword">
+          <Form.Label>Country</Form.Label>
+          <Form.Control  placeholder="Enter Country" />
+        </Form.Group>
+ 
+        <Button variant="primary" type="submit">
+          Continue
+        </Button>
+      </Form>
     </>
   )
 }
